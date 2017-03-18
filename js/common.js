37d05a15e43f59ae9f3c5736dcc4a42aeae382c1
SendSmile <dpaschenko94@gmail.com>
2017-03-18 14:26:13 +0200

    sef

--- js/common.js
+++ js/common.js
@@ -1,6 +1,15 @@
-// Can also be used with $(document).ready()
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
+$(document).ready(function() { 
+var slider = $('.bxslider').bxSlider({
+  mode: 'horizontal'
+});
+
+$('#reload-slider').click(function(e){
+  e.preventDefault();
+  slider.reloadSlider({
+    mode: 'fade',
+    auto: true,
+    pause: 4000,
+    speed: 700
   });
 });
+})
